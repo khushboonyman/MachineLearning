8b341088fc3a20131bcc06a4431cdacabe7c6956
khushboo <khushii.me@gmail.com>
2019-11-09 15:13:19 +0100

    fixed some regression errors

--- regressionpatrtB.py
+++ regressionpatrtB.py
@@ -8,23 +8,15 @@ import sklearn.linear_model as lm
 from sklearn import model_selection
 from toolbox_02450 import rlr_validate
 
+K = 2
+internal_cross_validation = 2
 # Add offset attribute
 X = np.concatenate((np.ones((X.shape[0],1)),X),1)
 attributeNames = [u'Offset']+attributeNames
 M = M+1
-
-## Crossvalidation
-# Create crossvalidation partition for evaluation
-K = 2
-internal_cross_validation = 2
-CV = model_selection.KFold(K, shuffle=True)
-#CV = model_selection.KFold(K, shuffle=False)
-
-# Values of lambda
+#PREPARE REGRESSION
 lambdas = np.power(10.,range(-5,9))
-
-# Initialize variables
-#T = len(lambdas)
+# Initialize variables    
 Error_train_rlr = np.empty((K,1))
 Error_test_rlr = np.empty((K,1))
 Error_train_nofeatures = np.empty((K,1))
@@ -32,11 +24,15 @@ Error_test_nofeatures = np.empty((K,1))
 w_rlr = np.empty((M,K))
 mu = np.empty((K, M-1))
 sigma = np.empty((K, M-1))
-w_noreg = np.empty((M,K))
+
+## Crossvalidation
+# Create crossvalidation partition for evaluation
+CV = model_selection.KFold(K, shuffle=True)    
 
 k=0
+train_test_index = []
 for train_index, test_index in CV.split(X,y):
-    
+    train_test_index.append([train_index,test_index])
     X_train = X[train_index]
     y_train = y[train_index]
     X_test = X[test_index]
@@ -46,12 +42,9 @@ for train_index, test_index in CV.split(X,y):
 
     mu[k, :] = np.mean(X_train[:, 1:], 0)
     sigma[k, :] = np.std(X_train[:, 1:], 0)
-    print("internal loop",k,mu,sigma)
-    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
-        
-    Xty = X_train.T @ y_train
-    XtX = X_train.T @ X_train
+    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :]  
+    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :]        
+    Xty = X_train.T @ y_train ; XtX = X_train.T @ X_train
     
     # Compute mean squared error without using the input data at all
     Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
@@ -73,9 +66,6 @@ for train_index, test_index in CV.split(X,y):
         xlabel('Regularization factor')
         ylabel('Mean Coefficient Values')
         grid()
-        # You can choose to display the legend, but it's omitted for a cleaner 
-        # plot, since there are many attributes
-        #legend(attributeNames[1:], loc='best')        
         subplot(1,2,2)
         title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
         loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
@@ -83,10 +73,6 @@ for train_index, test_index in CV.split(X,y):
         ylabel('Squared error (crossvalidation)')
         legend(['Train error','Validation error'])
         grid()    
-    # To inspect the used indices, use these print statements
-    #print('Cross validation fold {0}/{1}:'.format(k+1,K))
-    #print('Train indices: {0}'.format(train_index))
-    #print('Test indices: {0}\n'.format(test_index))
 
     k+=1
 
